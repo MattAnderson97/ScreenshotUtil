12720d7b8e2e417aa783c85c8c76f34add690082
Wolv <pragfang@gmail.com>
2017-10-11 14:51:50 +0100

    add missing title

--- screenshot.py
+++ screenshot.py
@@ -2,15 +2,15 @@ import pyscreenshot as ImageGrab
 
 def screen(imageTitle):
     im = ImageGrab.grab()
-    save(im)
+    save(im, imageTitle)
 
 
 def selection(imageTitle):
     bbox = None
     im = ImageGrab.grab(bbox = bbox)
-    save(im)
+    save(im, imageTitle)
 
 
-def save(image):
-    image.save()
+def save(image, title):
+    image.save(title)
     print("screenshot saved")
