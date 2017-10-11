c2b7e0d8f2f95aa81444d4025ceee92098f68e79
Wolv <pragfang@gmail.com>
2017-10-11 14:49:28 +0100

    remove repetition in screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -2,12 +2,15 @@ import pyscreenshot as ImageGrab
 
 def screen(imageTitle):
     im = ImageGrab.grab()
-    im.save(imageTitle)
-    print("screenshot saved")
+    save(im)
 
 
 def selection(imageTitle):
     bbox = None
     im = ImageGrab.grab(bbox = bbox)
-    im.save(imageTitle)
+    save(im)
+
+
+def save(image):
+    image.save()
     print("screenshot saved")
